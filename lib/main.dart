4c441dd89fb54f740947a0ca936a8aed648915b8
John Rollins <john.rollinsjr07@gmail.com>
2025-02-12 19:18:26 -0500

    Yo I did somethign please don tbe angry

--- lib/main.dart
+++ lib/main.dart
@@ -37,34 +37,50 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: Text('Valentine\'s Images'),
-          backgroundColor: const Color.fromARGB(255, 186, 149, 182), // Set title background color
+          backgroundColor: const Color.fromARGB(
+              255, 186, 149, 182), // Set title background color
           bottom: TabBar(
             indicatorColor: Colors.orange, // Set tab indicator color
-            labelColor: const Color.fromARGB(255, 255, 255, 255), // Set selected tab text color
-            unselectedLabelColor: Colors.black54, // Set unselected tab text color
+            labelColor: const Color.fromARGB(
+                255, 255, 255, 255), // Set selected tab text color
+            unselectedLabelColor:
+                Colors.black54, // Set unselected tab text color
             tabs: [
               Tab(text: 'Heartbeat'),
               Tab(text: 'Floating Hearts'),
+              Tab(text: 'Wavey Heart')
             ],
           ),
         ),
         body: TabBarView(
           children: [
             Center(
-              child: Image.asset("assets/images/heartbeat.gif",
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: double.infinity,),
+              child: Image.asset(
+                "assets/images/heartbeat.gif",
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
             ),
             Center(
-              child: Image.asset("assets/images/heartsfloating.gif",
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: double.infinity,),
+              child: Image.asset(
+                "assets/images/heartsfloating.gif",
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+            Center(
+              child: Image.asset(
+                "assets/images/waveyHeart.gif",
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
             ),
           ],
         ),
